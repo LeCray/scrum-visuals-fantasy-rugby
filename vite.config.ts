b75c168a8a7d5a66ee96692f14e34a5cefcabb51
Remy Roel <112566889+remyroel@users.noreply.github.com>
2025-04-24 15:44:57 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production"
+    ? "/scrum-visuals-fantasy-rugby-main/"
+    : "/",
   server: {
     host: "::",
     port: 8080,
